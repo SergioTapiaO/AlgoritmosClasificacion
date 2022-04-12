61b6f52845cff4c3e0334a69036cf6ecbea26aa7
SergioTapiaO <sergiotopers@gmail.com>
2022-04-12 20:06:05 +0200

    lloyd casi acabado

--- Lloyd.js
+++ Lloyd.js
@@ -3,18 +3,28 @@ class Lloyd{
     constructor(matriz, inicializacion){
         this.E = 10 ** (-10);
         this.K_MAX = 10;
-        this.GAGMA = 0.1;
+        this.GAMMA = 0.1;
         this.matriz = matriz;
-        //aqui guardamos los centros originales
-        this.centros_ini = inicializacion;
         // aqui guardamos los centros finales
         this.centros = inicializacion;
+        //aqui guardamos los centros originales
+        this.centros_ini = this.copiar();
+    }
+
+    calcular(){
+        let iteraciones = 0;
+        let fin = false;
+        do{
+            this.algoritmo();
+            //comprobamos la variacion de los centros
+            fin = this.comprobar_centros();
+        }while(iteraciones < this.K_MAX || !fin);
     }
 
     algoritmo(){
         //para tantas muestras como filas tenga la matriz
         for(let i = 0; i < this.matriz.length; i++){
-            let ind = this.menor_distancia(this.matriz[i]);
+            this.menor_distancia(this.matriz[i]);
         }
     }
 
@@ -30,15 +40,56 @@ class Lloyd{
             }
         } //for externo
 
+        //actualizamos el centro en funcion del menor
+        this.actualizar_centro(ind, muestra);
     }
 
     distancia_muestra(centro, muestra){
         let acum = 0;
         for(let j = 0; j < muestra.length - 1; j++){
-            let resta = centro[j] - muestra[j];
+            let resta = centro[j] - parseFloat(muestra[j]);
             resta = Math.pow(resta, 2);
             acum = acum + resta;
         }
+        return acum;
+    }
+
+    actualizar_centro(ind, muestra){
+        for(let i = 0; i < this.centros[ind].length; i++){
+            this.centros[ind][i] = this.centros[ind][i] + this.GAMMA*(parseFloat(muestra[ind]) - this.centros[ind][i]);
+        }
+    }
+
+    comprobar_centros(){
+        //con que una no cumpla se vuelve a iterar
+        let final = true;
+        for(let i = 0; i < this.centros.length; i++){
+            if(!this.variacion_centros(this.centros[i], this.centros_ini[i]))
+                final = false;
+        }
+        return final;
+    }
+
+    variacion_centros(centros, centros_ini){
+        let resul = 0;
+        for(let i = 0; i < centros.length;i++){
+            resul = resul + Math.pow( centros[i] - centros_ini[i], 2);
+        }
+
+        if(Math.sqrt(resul) < this.E)
+            return true;
+        else return false;
+    }
+
+    copiar(){
+        let ini = new Array();
+        for(let i = 0; i < this.centros.length; i++){
+            ini[i] = new Array();
+            for(let j = 0; j < this.centros[i].length; j++){
+                
+            }
+        }
+        return ini;
     }
 
 }
--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var contenido = null;
 var matriz = new Array();
 // SACADO DEL ENUNCIADO 
 var centros = new Array();
+var inicializacion = new Array();
 inicializacion[0] = new Array(4.6, 3.0, 4.0, 0.0);
 inicializacion[1] = new Array(6.8, 3.4, 4.6, 0.7);
 
@@ -30,4 +31,5 @@ function leerFichero(){
 
 function lloyd(){
     let sol_lloyd = new Lloyd(matriz, inicializacion);
+    sol_lloyd.calcular();
 }
