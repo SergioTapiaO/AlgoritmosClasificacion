7c8cb5dc038771ec2540f9f3909b9db8f44e98b5
SergioTapiaO <sergiotopers@gmail.com>
2022-04-12 13:14:17 +0200

    algoritmo lloyd empezado

--- Lloyd.js
+++ Lloyd.js
@@ -5,11 +5,40 @@ class Lloyd{
         this.K_MAX = 10;
         this.GAGMA = 0.1;
         this.matriz = matriz;
-        this.inicializacion = inicializacion;
+        //aqui guardamos los centros originales
+        this.centros_ini = inicializacion;
+        // aqui guardamos los centros finales
+        this.centros = inicializacion;
     }
 
     algoritmo(){
-        
+        //para tantas muestras como filas tenga la matriz
+        for(let i = 0; i < this.matriz.length; i++){
+            let ind = this.menor_distancia(this.matriz[i]);
+        }
+    }
+
+    menor_distancia(muestra){
+        let menor = 100;
+        let ind = -1;
+        //para cada centro
+        for(let i = 0; i < this.centros.length; i++){
+            let dist = this.distancia_muestra(this.centros[i], muestra);
+            if(dist < menor){
+                menor = dist;
+                ind = i;
+            }
+        } //for externo
+
+    }
+
+    distancia_muestra(centro, muestra){
+        let acum = 0;
+        for(let j = 0; j < muestra.length - 1; j++){
+            let resta = centro[j] - muestra[j];
+            resta = Math.pow(resta, 2);
+            acum = acum + resta;
+        }
     }
 
 }
